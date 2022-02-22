eccbc2ff430229998d1bb322b109adc964986c30
Kris Vanneste <kris.vanneste@oma.be>
2022-02-22 10:13:26 +0100

    Updated imports. Fixed max_mag of grid_src_model. Added additional constraint with partial positive evidence. Change content of text box as function of method. Set 'rms_is_prob' argument as function of method in plot_gridsearch_map call. Improved naming of figure file.

--- plot_prob_maps_bw1997.py
+++ plot_prob_maps_bw1997.py
@@ -9,17 +9,19 @@ from hazard.rshalib.source_estimation import estimate_epicenter_location_and_mag
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(SCRIPT_DIR)
-from castorlib import (plot_gridsearch_map, 
+from castorlib import (plot_gridsearch_map,
 					   read_evidence_site_info_from_txt,
-					   TRT, USD, LSD, RAR, RMS, 
+					   read_evidence_sites_from_gis,
+					   TRT, USD, LSD, RAR, RMS,
 					   project_folder, gis_folder,
-					   data_points, fault_model)
+					   data_points, fault_model,
+					   base_fig_folder)
 
 
-base_fig_folder = os.path.join(project_folder, "Projects", "Castor-Pollux", "Figures")
 fig_folder = os.path.join(base_fig_folder)
 if not os.path.exists(fig_folder):
-	os.mkdir(fig_folder)
+	print('Figures folder does not exist')
+	#os.mkdir(fig_folder)
 
 output_format = 'png'
 
@@ -45,7 +47,7 @@ point_msr = oqhazlib.scalerel.PointMSR()
 wc1994_msr = oqhazlib.scalerel.WC1994()
 
 grd_src_model = SimpleUniformGridSourceModel(grid_outline, grid_spacing,
-			min_mag, min_mag + mag_bin_width, mag_bin_width, depth,
+			min_mag, max_mag + mag_bin_width, mag_bin_width, depth,
 			strike, dip, rake, wc1994_msr, USD, LSD, RMS, RAR, TRT)
 lon_grid, lat_grid = grd_src_model.lon_grid, grd_src_model.lat_grid
 
@@ -79,11 +81,24 @@ for nesm, ne_threshold in zip(ne_site_models, ne_thresholds):
 pe_thresholds = np.array(pe_thresholds)
 ne_thresholds = np.array(ne_thresholds)
 
+## Additional constraint: intensities must be 7.5 or higher in part of Aysén catchment
+gis_file = os.path.join(gis_folder, 'Aysen_Watershed_fixed2.shp')
+site_spacing = 10
+partial_pe_site_model = read_evidence_sites_from_gis(gis_file, site_spacing)[0]
+partial_pe_sites = partial_pe_site_model.get_sites()
+partial_pe_thresholds = [7.5] * len(partial_pe_sites)
+partial_pe_fraction = 0.35
+
+
+
 ## Compute magnitudes and RMS errors at grid points
+method = 'forward'
 (mag_grid, rms_grid) = (
 	estimate_epicenter_location_and_magnitude_from_intensities(
 	ipe_name, imt, grd_src_model, pe_sites, pe_thresholds,
-	ne_sites, ne_thresholds, method="reverse", mag_bounds=(min_mag, max_mag)))
+	ne_sites, ne_thresholds, method=method,
+	partial_pe_sites=partial_pe_sites, partial_pe_intensities=partial_pe_thresholds,
+	partial_pe_fraction=partial_pe_fraction))
 idx = np.unravel_index(rms_grid.argmin(), rms_grid.shape)
 #print(mag_grid[idx], lon_grid[idx], lat_grid[idx])
 
@@ -93,17 +108,24 @@ rms_grid[np.isinf(rms_grid)] = 10.0
 ## Plot map
 # TODO: blend alpha in function of rms
 # See: https://matplotlib.org/devdocs/gallery/images_contours_and_fields/image_transparency_blend.html
-text_box = "Event: %s\nRMSE: %.2f - %.2f"
-text_box %= (event, rms_grid.min(), rms_grid[rms_grid < 10].max())
+text_box = "Event: %s\n" % event
+if 'probabilistic' in method:
+	text_box += "P: %.2f - %.2f"
+else:
+	text_box += "RMSE: %.2f - %.2f"
+text_box %= (rms_grid.min(), rms_grid[rms_grid < 10].max())
 
+rms_is_prob = ('probabilistic' in method)
 map = plot_gridsearch_map(grd_src_model, mag_grid, rms_grid,
 						pe_site_models, ne_site_models,
 						site_model_gis_file=None,
 						text_box=text_box,
-						plot_rms_as_alpha=False, plot_epicenter_as="area")
+						plot_rms_as_alpha=False, rms_is_prob=rms_is_prob,
+						plot_epicenter_as="area")
 
-fig_filespec = os.path.join(fig_folder, "%s_bw1997.%s" % (event, output_format))
-#fig_filespec = None
+fig_filespec = os.path.join(fig_folder, "%s_%s_%s+watershed_10%%.%s")
+fig_filespec %= (event, ipe_name, method, output_format)
+fig_filespec = None
 
 dpi = 200 if fig_filespec else 90
 map.plot(fig_filespec=fig_filespec, dpi=dpi)
